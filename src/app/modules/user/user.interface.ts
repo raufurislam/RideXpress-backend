adbf843b49b205b3835a1c0b2d2f97a4107b0bac
Raufur Islam <raufurislam@gmail.com>
2025-07-29 15:42:56 +0600

    User init

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,35 @@
+import { Types } from "mongoose";
+
+export enum Role {
+  SUPER_ADMIN = "SUPER_ADMIN",
+  ADMIN = "ADMIN",
+  RIDER = "RIDER",
+  DRIVER = "DRIVER",
+}
+
+export interface IAuthProvider {
+  provider: "google" | "credentials";
+  providerId: string;
+}
+
+export enum IsActive {
+  ACTIVE = "ACTIVE",
+  BLOCKED = "BLOCK",
+  SUSPENDED = "SUSPENDED",
+}
+
+export interface IUser {
+  _id?: Types.ObjectId;
+  name: string;
+  email: string;
+  picture?: string;
+  password?: string;
+  phone?: string;
+  address?: string;
+  auths: IAuthProvider[];
+  role: Role;
+  isDeleted?: string;
+  isActive?: IsActive;
+  isVerified?: boolean;
+  rides?: Types.ObjectId[];
+}
