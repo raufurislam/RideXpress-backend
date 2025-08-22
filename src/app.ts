ba6fe081db43ae6fc2e1c9a4e9fbeabe4b9a906f
raufurislam <raufurislam@gmail.com>
2025-08-22 23:27:46 +0600

    Initially ready for frontend environment

--- src/app.ts
+++ src/app.ts
@@ -24,7 +24,12 @@ app.use(cookieParser());
 app.use(express.json());
 app.set("trust proxy", 1);
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: envVars.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use("/api/v1", router);
 
