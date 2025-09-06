56beb34f27c5ba2955e4a593da062c33c85dca6d
raufurislam <raufurislam@gmail.com>
2025-09-06 21:08:08 +0600

    Deployment ready both frontend and backend

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.set("trust proxy", 1);
 app.use(express.urlencoded({ extended: true }));
+
 app.use(
   cors({
     origin: envVars.FRONTEND_URL,
--- src/app/utils/setCookie.ts
+++ src/app/utils/setCookie.ts
@@ -1,15 +1,10 @@
 // setCookie.ts
 import { Response } from "express";
-import { envVars } from "./../config/env";
-import ms from "ms";
 
 export interface AuthTokens {
   accessToken?: string;
   refreshToken?: string;
 }
-
-const accessMaxAge = ms(envVars.JWT_ACCESS_EXPIRES as ms.StringValue);
-const refreshMaxAge = ms(envVars.JWT_REFRESH_EXPIRES as ms.StringValue);
 
 export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
   if (tokenInfo.accessToken) {
@@ -22,7 +17,6 @@ export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
       httpOnly: true,
       secure: true,
       sameSite: "none",
-      maxAge: accessMaxAge,
     });
   }
 
@@ -36,7 +30,49 @@ export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
       httpOnly: true,
       secure: true,
       sameSite: "none",
-      maxAge: refreshMaxAge,
     });
   }
 };
+
+// // setCookie.ts
+// import { Response } from "express";
+// import { envVars } from "./../config/env";
+// import ms from "ms";
+
+// export interface AuthTokens {
+//   accessToken?: string;
+//   refreshToken?: string;
+// }
+
+// const accessMaxAge = ms(envVars.JWT_ACCESS_EXPIRES as ms.StringValue);
+// const refreshMaxAge = ms(envVars.JWT_REFRESH_EXPIRES as ms.StringValue);
+
+// export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
+//   if (tokenInfo.accessToken) {
+//     res.cookie("accessToken", tokenInfo.accessToken, {
+//       // httpOnly: true,
+//       // // secure: envVars.NODE_ENV === "production",
+//       // secure: true,
+//       // sameSite: "none",
+
+//       httpOnly: true,
+//       secure: true,
+//       sameSite: "none",
+//       maxAge: accessMaxAge,
+//     });
+//   }
+
+//   if (tokenInfo.refreshToken) {
+//     res.cookie("refreshToken", tokenInfo.refreshToken, {
+//       // httpOnly: true,
+//       // // secure: envVars.NODE_ENV === "production",
+//       // secure: true,
+//       // sameSite: "none",
+
+//       httpOnly: true,
+//       secure: true,
+//       sameSite: "none",
+//       maxAge: refreshMaxAge,
+//     });
+//   }
+// };
